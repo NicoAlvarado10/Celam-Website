21454a13586e381dd68d735146e95aa197951c73
moviedo <marcelo.oviedo@gmail.com>
2025-10-06 12:23:19 -0300

    article

--- src/sections/home/accesosPrincipales/AccesoItem2.tsx
+++ src/sections/home/accesosPrincipales/AccesoItem2.tsx
@@ -10,44 +10,31 @@ type Props = {
 
 export default function AccesoItem2({ label, path, icon }: Props) {
     return (
-        <article className="flex flex-col items-center gap-2 group cursor-pointer ">
+        <div className="group relative w-44 h-44 border-slate-300  rounded-full">
+            {/* burbuja */}
+            <div
+                className="absolute left-0 top-0 w-16 h-16 rounded-full bg-sky-200/90
+               transition-all duration-900 ease-in-out
+               group-hover:left-1/2 group-hover:top-1/2
+               group-hover:-translate-x-1/2 group-hover:-translate-y-1/2
+               group-hover:w-full group-hover:h-full
+               group-hover:bg-orange-400/15
+               z-[9999]"   // 🔥 siempre encima
+            ></div>
 
-            <div className="relative w-44 h-44  bg-gray-100 border border-slate-300 overflow-hidden  rounded-full">
-
-                {/* burbuja */}
-                <div className="absolute
-                        pointer-events-none 
-                         z-50
-                                                 -top-5 left-3
-                        w-16 h-16 
-                        rounded-full
-                        bg-sky-200/90
-                        transition-all 
-                        duration-900 
-                        ease-in-out
-                        group-hover:left-1/2 
-                        group-hover:top-1/2
-                        group-hover:-translate-x-1/2 
-                        group-hover:-translate-y-1/2
-                        group-hover:w-full 
-                        group-hover:h-full
-                        group-hover:bg-orange-400/15
-                        mix-blend-normal
-                        ">
-                </div>
-                <Link
-                    to={path}
-                    className="relative z-10 flex flex-col items-center justify-center
+            {/* contenido */}
+            <Link
+                to={path}
+                className="relative z-10 flex flex-col items-center justify-center
                gap-2 w-10/12 mx-auto text-center"
-                >
-                    <i className={`ri-${icon} text-6xl max-sm:text-5xl `} />
-                    <h3 className="text-sm font-semibold uppercase tracking-wide leading-tight break-words">
-                        {label}
-                    </h3>
-                </Link>
+            >
+                <i className={`ri-${icon} text-6xl max-sm:text-5xl `} />
+                <h3 className="text-sm font-semibold uppercase tracking-wide leading-tight break-words">
+                    {label}
+                </h3>
+            </Link>
+        </div>
 
-
-            </div>
-        </article>
+       
     )
 }
