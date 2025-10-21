f7b08351d3c3c65629419e648c6a7ae27b315754
Nicolás Alvarado <alvaradonicolas317@gmail.com>
2025-10-21 16:58:35 -0300

    feat: ajustes en accesos-principales

--- src/sections/home/accesosPrincipales/AccesoItem2.tsx
+++ src/sections/home/accesosPrincipales/AccesoItem2.tsx
@@ -12,7 +12,7 @@ export default function AccesoItem2({ label, path, icon }: Props) {
       to={path}
       className="group relative isolate  rounded-full mx-auto flex flex-col items-center justify-center"
     >
-      <span className="relative z-50 flex flex-col items-center justify-center gap-2 w-10/12 mx-auto text-center text-blue-900">
+      <span className="relative z-50 flex flex-col items-center justify-center gap-2 w-10/12 mx-auto text-center text-blue-900 group-hover:text-[#FB5900]/70">
         {/* Ícono con burbuja animada al costado */}
         <i
           className={`ri-${icon} text-6xl max-sm:text-5xl relative z-20
@@ -20,12 +20,12 @@ export default function AccesoItem2({ label, path, icon }: Props) {
             after:w-10 after:h-10 after:rounded-full after:bg-sky-200/90
             after:transition-all after:duration-700 after:ease-in-out
             group-hover:after:left-1/2 group-hover:after:-translate-x-1/2
-            group-hover:after:w-20 group-hover:after:h-20 group-hover:after:bg-sky-400/30
+            group-hover:after:w-20 group-hover:after:h-20 group-hover:after:bg-[#FB5900]/20
             after:z-10`}
         />
 
         {/* Texto sin animación */}
-        <h3 className="text-sm font-semibold group-hover:text-blue-400/90 transition-all m-4 uppercase tracking-wide leading-tight break-words">
+        <h3 className="text-sm font-semibold group-hover:text-[#FB5900] transition-all m-4 uppercase tracking-wide leading-tight break-words">
           {label}
         </h3>
       </span>
