3f93b7f19a2c2cf757511759f8e7d3c5f59d64e0
Nicolás Alvarado <alvaradonicolas317@gmail.com>
2025-10-22 18:19:31 -0300

    feat(Carrusel): agregar nuevas imágenes y ajustar estilos de presentación en el componente Carrusel

--- src/components/common/Carrusel.tsx
+++ src/components/common/Carrusel.tsx
@@ -1,6 +1,6 @@
 // common/Carrusel.tsx
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import {  Autoplay } from "swiper/modules";
 
 import Botton from "./Botton";
 
@@ -16,30 +16,29 @@ interface CarruselProps {
 
 export const Carrusel: React.FC<CarruselProps> = ({ slides }) => {
   return (
-    <div className="w-full max-w-4xl mx-auto">
+    <div className="w-full  max-w-7xl mx-auto">
       <Swiper
-        modules={[Navigation, Pagination, Autoplay]}
-        navigation
-        pagination={{ clickable: true }}
+        modules={[ Autoplay]}
+        
         autoplay={{ delay: 4000 }}
         loop
         className="rounded-xl overflow-hidden"
       >
         {slides.map((slide, index) => (
           <SwiperSlide key={index}>
-            <div className="flex flex-col items-center justify-center gap-4 p-4">
+            <div className="flex flex-col items-center justify-center gap-4 ">
               <img
                 src={slide.imageSrc}
                 alt={`Slide ${index + 1}`}
-                className="w-full h-64 object-cover rounded-lg"
+                className="w-full h-[350px] max-md:h-[200px] object-cover rounded-lg"
               />
               <Botton
                 href={slide.buttonHref}
                 variant="gradient"
-                className="mt-2"
+                className=" rounded-sm text-xl "
               >
                 {slide.buttonLabel}
-                <i className="ri-arrow-right-line ml-2 text-base" />
+                
               </Botton>
             </div>
           </SwiperSlide>
